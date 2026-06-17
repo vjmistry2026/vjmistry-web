df703f6ece2858abfefe85cda4a46a40e7f8668b
rahees <raheez1995india@gmail.com>
2026-06-17 19:08:03 +0530

    footer and founder message changes

--- app/components/client/about/founder-message/Index.tsx
+++ app/components/client/about/founder-message/Index.tsx
@@ -1,11 +1,17 @@
 import { FoundersMessageType } from "@/app/types/foundersMessage";
 import CorePhilosophy from "./sections/CorePhilosophy";
-import FoundersMessage from "./sections/FounderMessage";
+// import FoundersMessage from "./sections/FounderMessage";
+import PageBanner from "../../common/PageBanner";
+import Main from "./sections/Main";
+
+
 
 const Index = ({ data }: { data: FoundersMessageType }) => {
     return (
         <>
-            <FoundersMessage data={data.firstSection} />
+            <PageBanner title={"Founder’s Message"} image={"/assets/images/founder-message/banner.jpg"} imageAlt={"founder's message"} />
+            <Main data={data.firstSection} />
+            {/* <FoundersMessage data={data.firstSection} /> */}
             <CorePhilosophy data={data.secondSection} />
         </>
     );
