f00e604d9b0291dcf354579b260889bd1b6d1c55
Nived <nived@globalsurf.ae>
2026-07-24 15:54:47 +0530

    fix:current open project displayed in related projects

--- app/components/client/project-details/Index.tsx
+++ app/components/client/project-details/Index.tsx
@@ -6,12 +6,13 @@ import RelatedProjects from "./sections/RelatedProjects";
 import { ProjectType } from "@/app/types/project";
 
 const Index = ({ project, allProject }: { project: ProjectType['projects'][number], allProject: ProjectType }) => {
+    console.log(project)
     return (
         <>
             <PageBanner title={project.title} image={project.banner} imageAlt={project.bannerAlt} />
             <ProjectDescription firstSection={project.firstSection} secondSection={project.secondSection} />
             <VideoSection data={project.thirdSection} />
-            <RelatedProjects data={allProject} />
+            <RelatedProjects data={allProject} currentProject={project.slug}/>
         </>
     );
 };
